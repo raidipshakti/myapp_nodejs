1de2834546fdac8df2b796f13a10476756d13668
raidipshakti <rai.dipshakti@gmail.com>
2026-07-01 13:29:21 +0545

    initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+// Import Express framework
+const express = require("express");
+
+// Create Express application
+const app = express();
+
+// Application Port
+const PORT = 3000;
+
+// Home Route
+app.get("/", (req, res) => {
+    res.send("<h1>Welcome to NodeJS Family</h1>");
+});
+
+// Start Server
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
